4b1e59c4bf937194f95f70184016725445ab7da2
Natalia <27_nata_rus@mail.ru>
2020-10-29 18:19:45 +0300

    Commented out the console.log

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -11,5 +11,5 @@ function truncate(str, maxlength) {
   return str.slice(0, (maxlength - 1)) + '…';
 }
 
- console.log( truncate("Вот, что мне хотелось бы сказать на эту тему:", 21) );
- console.log( truncate("Всем привет!", 20) );
+ // console.log( truncate("Вот, что мне хотелось бы сказать на эту тему:", 21) );
+ // console.log( truncate("Всем привет!", 20) );
