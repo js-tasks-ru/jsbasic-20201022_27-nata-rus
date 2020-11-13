edfa193534656a2321b4fcda56f35be16b8359b2
Natalia <27_nata_rus@mail.ru>
2020-11-13 16:14:38 +0300

    Added Camelize function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,13 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  return str
+  .split('-')
+  .map( (strUpper, index) => index == 0 ? strUpper : strUpper[0].toUpperCase() + strUpper.slice(1) )
+  .join('');
 }
+
+
+// console.log( camelize('background-color') );
+// console.log( camelize('list-style-image') );
+// console.log( camelize('-webkit-transition') );
