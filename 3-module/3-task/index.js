3ea67a1439d72f1b35d4a0d6b8f35c16b65c2c0b
Natalia <27_nata_rus@mail.ru>
2020-11-13 16:16:51 +0300

    Added Camelize function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,12 +3,13 @@
  * @returns {string}
  */
 function camelize(str) {
+
   return str
   .split('-')
   .map( (strUpper, index) => index == 0 ? strUpper : strUpper[0].toUpperCase() + strUpper.slice(1) )
   .join('');
-}
 
+}
 
 // console.log( camelize('background-color') );
 // console.log( camelize('list-style-image') );
