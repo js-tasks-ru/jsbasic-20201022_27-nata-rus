bfb532819a141fe0d46f2b18730d910bf4790cf9
Natalia <27_nata_rus@mail.ru>
2020-11-21 20:30:59 +0300

    Function for TableTeachers

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,30 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+    for (let key = 0; key < table.rows.length; key++) {
+
+        let row = table.rows[key];
+
+        if (row.cells[1].innerHTML < 18) {
+          row.style = 'text-decoration: line-through';
+        }
+
+        if (row.cells[2].innerHTML === 'f') {
+          row.classList.add('female');
+        } else {
+          row.classList.add('male');
+        }
+
+        if (row.cells[3].getAttribute('data-available') === 'true') {
+          row.classList.add('available');
+        } else if (row.cells[3].getAttribute('data-available') === null) {
+          row.setAttribute('hidden', '');
+        } else {
+          row.classList.add('unavailable');
+        }
+      }
+
+      
+
 }
