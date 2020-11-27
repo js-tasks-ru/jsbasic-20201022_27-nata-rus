67733b9f12b0682b0901450b2247a96158815ce5
Natalia <27_nata_rus@mail.ru>
2020-11-27 14:02:17 +0300

    Create UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -29,6 +29,45 @@
  * @constructor
  */
 export default class UserTable {
+
   constructor(rows) {
+    this.rows = rows;
+    this.table = document.createElement('table');
+    this.table.addEventListener('click', (event) => this.onClick(event));
+    this.render(rows);
+  }
+  
+  get elem() {
+    return this.table;
+  }
+
+  render(items) {
+    
+    let tableHead = `<thead>
+     <tr>
+       <th>Имя</th>
+       <th>Возраст</th>
+       <th>Зарплата</th>
+       <th>Город</th>
+       <th></th>
+     </tr>
+    </thead>`;
+
+    let tableCreate = items.map(function (item) {
+      return `<tr>
+           <td>${item.name}</td>
+           <td>${item.age}</td>
+           <td>${item.salary}</td>
+           <td>${item.city}</td>
+           <td><button>X</button></td>
+          </tr>`
+    }).join("");
+    this.table.innerHTML = tableHead + tableCreate;
+  }
+
+  onClick(event) {
+    if (event.target.closest('button')) {
+      event.target.closest('tr').remove();
+    }
   }
 }
